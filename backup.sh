0787543f4f088883e5b8c72380e2fc4fb186806e
kimumusan <kimumusan@gmail.com>
2017-04-09 21:24:05 +0900

    Add shellscripts to backup and restore

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run --rm \
+ -v collabotoolsdocker_redmine_filevolume:/target/redmine_file/ \
+ -v collabotoolsdocker_redmine_dbvolume:/target/redmine_db/ \
+ -v collabotoolsdocker_rocketchat_dbvolume:/target/rocketchat_db/ \
+ -v collabotoolsdocker_gitbucket_repovolume:/target/gitbucket_repo/ \
+ -v collabotoolsdocker_gitbucket_dbvolume:/target/gitbucket_db/ \
+ -v /$(pwd):/backup/ \
+ -i \
+ ubuntu tar cvfz //backup/backup.tar.gz //target
+
