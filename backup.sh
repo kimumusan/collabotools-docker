9669c0ef7ec431170a02d84bace322227de52dd3
kimumusan <kimumusan@gmail.com>
2017-04-29 22:16:44 +0900

    Add redmine-plugin backup

--- backup.sh
+++ backup.sh
@@ -4,12 +4,13 @@ docker-compose stop
 
 docker run --rm \
  -v collabotoolsdocker_redmine_filevolume:/target/redmine_file/ \
+ -v collabotoolsdocker_redmine_pluginvolume:/target/redmine_plugin/ \
  -v collabotoolsdocker_redmine_dbvolume:/target/redmine_db/ \
  -v collabotoolsdocker_rocketchat_dbvolume:/target/rocketchat_db/ \
  -v collabotoolsdocker_gitbucket_repovolume:/target/gitbucket_repo/ \
  -v collabotoolsdocker_gitbucket_dbvolume:/target/gitbucket_db/ \
  -v /$(pwd):/backup/ \
  -i \
- ubuntu tar cvfz //backup/backup.tar.gz //target
+ ubuntu tar cvfz //backup/backup.tar.gz //target 1> backup.log 2> backup_error.log
 
 docker-compose start
--- restore.sh
+++ restore.sh
@@ -4,12 +4,13 @@ docker-compose stop
 
 docker run --rm\
  -v collabotoolsdocker_redmine_filevolume:/target/redmine_file/ \
+ -v collabotoolsdocker_redmine_pluginvolume:/target/redmine_plugin/ \
  -v collabotoolsdocker_redmine_dbvolume:/target/redmine_db/ \
  -v collabotoolsdocker_rocketchat_dbvolume:/target/rocketchat_db/ \
  -v collabotoolsdocker_gitbucket_repovolume:/target/gitbucket_repo/ \
  -v collabotoolsdocker_gitbucket_dbvolume:/target/gitbucket_db/ \
  -v /$(pwd):/backup/ \
  -i \
- ubuntu bash -c "cd /target && tar xvfz /backup/backup.tar.gz --strip 1"
+ ubuntu bash -c "cd /target && tar xvfz /backup/backup.tar.gz --strip 1" 1> restore.log 2> restore_error.log
 
 docker-compose start
