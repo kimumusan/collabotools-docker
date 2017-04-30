3951331b5610d9ed701673eff7d4a52f15326dd2
kimumusan <kimumusan@gmail.com>
2017-04-30 22:53:24 +0900

    Change restore process to clean data volumes at first

--- backup.sh
+++ backup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-docker-compose stop
+echo "Backup is started."
+date +"Backup is started in %Y/%m/%d %p at %I:%M:%S" 1> logs/backup.log 2> logs/backup_error.log
 
+echo "Stopping the containers..."
+docker-compose stop 1>> logs/backup.log 2>> logs/backup_error.log
+
+echo "Archiving data volumes..."
 docker run --rm \
  -v collabotoolsdocker_redmine_filevolume:/target/redmine_file/ \
  -v collabotoolsdocker_redmine_pluginvolume:/target/redmine_plugin/ \
@@ -11,6 +16,13 @@ docker run --rm \
  -v collabotoolsdocker_gitbucket_dbvolume:/target/gitbucket_db/ \
  -v /$(pwd):/backup/ \
  -i \
- ubuntu tar cvfz //backup/backup.tar.gz //target 1> logs/backup.log 2> logs/backup_error.log
+ ubuntu tar cvfz //backup/backup.tar.gz //target 1>> logs/backup.log 2>> logs/backup_error.log
 
-docker-compose start
+echo "Starting data the containers..."
+docker-compose start 1>> logs/backup.log 2>> logs/backup_error.log
+
+date +"Backup is finished in %Y/%m/%d %p at %I:%M:%S" 1>> logs/backup.log 2>> logs/backup_error.log
+
+echo "Backup is finished!"
+
+sleep 1s
--- restore.sh
+++ restore.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
-docker-compose stop
+echo "To restore, the containers will be recreated. Your changes to the containers can be disappeared. Do you want to continue? [Y/n]"
+read answer
 
+case $answer in 
+    "Y" | "y" | "yes" | "Yes" | "YES" )
+	;;
+    * )
+        echo "Restore is canceled."
+	exit
+	;;
+esac
+
+echo "Restore is started."
+date +"restore started in %Y/%m/%d %p  at %I:%M:%S" 1> logs/restore.log 2> logs/restore_error.log
+
+echo "Stopping and removing containers..."
+docker-compose down 1>> logs/restore.log 2>> logs/restore_error.log
+
+echo "Removing unused volumes..."
+docker volume prune --force 1>> logs/restore.log 2>> logs/restore_error.log
+
+echo "Creating new volumes and restoring data..."
 docker run --rm\
  -v collabotoolsdocker_redmine_filevolume:/target/redmine_file/ \
  -v collabotoolsdocker_redmine_pluginvolume:/target/redmine_plugin/ \
@@ -11,6 +31,13 @@ docker run --rm\
  -v collabotoolsdocker_gitbucket_dbvolume:/target/gitbucket_db/ \
  -v /$(pwd):/backup/ \
  -i \
- ubuntu bash -c "cd /target && tar xvfz /backup/backup.tar.gz --strip 1" 1> logs/restore.log 2> logs/restore_error.log
+ ubuntu bash -c "cd /target && tar xvfz /backup/backup.tar.gz --strip 1" 1>> logs/restore.log 2>> logs/restore_error.log
 
-docker-compose start
+echo "Creating and starting containers"
+docker-compose up -d 1>> logs/restore.log 2>> logs/restore_error.log
+
+date +"Restore is finished in %Y/%m/%d %p at %I:%M:%S"  1>> logs/restore.log 2>> logs/restore_error.log
+
+echo "Restore is finished!"
+
+sleep 1s
