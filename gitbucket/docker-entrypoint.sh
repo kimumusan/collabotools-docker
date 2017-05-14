ae7cf4f9f7b9fcc0575810e4574e6e5d5f8b6390
金翔海 <kimumusan@kinshoukainoMacBook-Pro.local>
2017-05-15 00:01:17 +0900

    Change git bucket repository from h2 to mysql

--- gitbucket/docker-entrypoint.sh
+++ gitbucket/docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [ "$GITBUCKET_DB_URL" -a "$GITBUCKET_DB_USER" -a "$GITBUCKET_DB_PASSWORD" ]; then
-  cat > /gitbucket/database.conf <<-EOCONF
+cat > /gitbucket/database.conf <<-EOCONF
 db {
-  url = "jdbc:mysql://$GITBUCKET_DB_HOST/$GITBUCKET_DB_NAME?useUnicode=true&characterEncoding=utf8" 
+  url = "jdbc:mysql://$GITBUCKET_DB_HOST/$GITBUCKET_DB_NAME?useUnicode=true&characterEncoding=utf8"
   user = "$GITBUCKET_DB_USER"
   password = "$GITBUCKET_DB_PASSWORD"
 }
 EOCONF
-fi
 
 echo "Wait until database $GITBUCKET_DB_HOST:3306 is ready..."
 until nc -z $GITBUCKET_DB_HOST 3306
@@ -19,4 +17,5 @@ done
 # Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
 sleep 1
 
-java -jar /opt/gitbucket.war
+echo "Starting gitbucket app"
+exec java $JAVA_OPTS -jar /opt/gitbucket.war $GITBUCKET_OPTS
